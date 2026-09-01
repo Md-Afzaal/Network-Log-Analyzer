292ff3425bd2c25bcb968dd7bf116360ee6a6aa7
Md-Afzaal <mohammadafzaal1332@gmail.com>
2026-09-01 22:17:54 +0530

    Add network analysis and formatted CLI output

--- src/main/java/com/networkanalyzer/Main.java
+++ src/main/java/com/networkanalyzer/Main.java
@@ -2,20 +2,16 @@ package com.networkanalyzer;
 
 import com.networkanalyzer.analyzer.LogAnalyzer;
 import com.networkanalyzer.model.AnalysisResult;
-import com.networkanalyzer.model.NetworkLog;
 import com.networkanalyzer.parser.LogParser;
 
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("----Network Analyzer----");
         LogParser logParser = new LogParser();
-        System.out.println(logParser.parse().get(0));
         LogAnalyzer logAnalyzer = new LogAnalyzer();
         AnalysisResult analyzer = logAnalyzer.analyze(logParser.parse());
-        System.out.println(analyzer.toString());
-//        System.out.println(logAnalyzer.analyze(logParser.parse()));
-//        System.out.println(logAnalyzer.totalLogs(logParser.parse()));
+//        int totalLogs = analyzer.getTotalLogs();
+        logAnalyzer.analysisResult(analyzer);
+//        System.out.println(analyzer.toString());
     }
 }
