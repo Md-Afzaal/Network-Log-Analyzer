bbd4fa5252b8ccf644974f3709401d0f3a6aeeac
Md-Afzaal <mohammadafzaal1332@gmail.com>
2026-09-06 17:52:13 +0530

    Cleaned up SortCLI

--- src/main/java/com/networkanalyzer/cli/SortCLI.java
+++ src/main/java/com/networkanalyzer/cli/SortCLI.java
@@ -45,25 +45,28 @@ public class SortCLI {
     }
     public void sortByTimeStamp(List<NetworkLog> logs,LogSorter logSorter,Scanner scan) {
         int choice = 0;
-        System.out.println("\n------------ Sort Order ------------\n");
-        System.out.println("1. Oldest to Newest\n"+
-                            "2. Newest to Oldest\n"+
-                            "3. Back\n");
-        choice = scan.nextInt();
-        switch (choice) {
-            case 1:
-                sortTimeStampByAscendingOrder(logs,logSorter);
-                break;
-            case 2:
-                sortTimeStampByDescendingOrder(logs,logSorter);
-                break;
-            case 3:
-                return;
-            default:
-                System.out.println("Invalid choice.");
-                break;
+        while(true){
+            System.out.println("\n------------ Sort Order ------------\n");
+            System.out.println("1. Oldest to Newest\n"+
+                    "2. Newest to Oldest\n"+
+                    "3. Back\n");
+            choice = scan.nextInt();
+            switch (choice) {
+                case 1:
+                    sortTimeStampByAscendingOrder(logs,logSorter);
+                    break;
+                case 2:
+                    sortTimeStampByDescendingOrder(logs,logSorter);
+                    break;
+                case 3:
+                    return;
+                default:
+                    System.out.println("Invalid choice.");
+                    return;
 
+            }
         }
+
     }
 
     public void sortTimeStampByAscendingOrder(List<NetworkLog> logs,LogSorter logSorter) {
