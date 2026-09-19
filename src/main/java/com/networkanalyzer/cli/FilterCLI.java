1b71f4abed4dde0f49ae072e7407b17fff4c836e
Md-Afzaal <mohammadafzaal1332@gmail.com>
2026-09-19 21:35:46 +0530

    Complete parser input validation

--- src/main/java/com/networkanalyzer/cli/FilterCLI.java
+++ src/main/java/com/networkanalyzer/cli/FilterCLI.java
@@ -2,13 +2,15 @@ package com.networkanalyzer.cli;
 
 import com.networkanalyzer.filter.LogFilter;
 import com.networkanalyzer.model.NetworkLog;
+import org.apache.commons.validator.routines.InetAddressValidator;
 
 import java.time.LocalDateTime;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class FilterCLI {
+
+    private final InetAddressValidator validator = InetAddressValidator.getInstance();
+
     public void filterLogsBy(List<NetworkLog> logs, LogFilter logFilter, Scanner scan){
         while(true){
             String target;
@@ -20,6 +22,7 @@ public class FilterCLI {
                     "5.Filter by Status\n"+
                     "6.Back to Main Menu\n");
             System.out.print("Enter your choice: ");
+
             try {
                 int choice = scan.nextInt();
                 if (choice == 6){
@@ -46,12 +49,17 @@ public class FilterCLI {
                         System.out.println("Invalid choice.");
                         continue;
                 }
+
                 target = scan.next();
-                if(choice == 4 && !isPortValid(target)){
-                    System.out.println("Invalid port. Enter a number between 0 and 65535.");
+
+                if(!isValidFilterInput(choice,target)){
+                    System.out.println("\nInvalid filter value.");
                     continue;
                 }
+
+                if(choice == 3 || choice == 5) target = target.toUpperCase();
                 filterResults(logs,logFilter,choice,target);
+
             } catch (InputMismatchException e){
                 System.out.println("Invalid input. Enter a number");
                 scan.nextLine();
@@ -59,20 +67,31 @@ public class FilterCLI {
         }
     }
 
-    private boolean isPortValid(String target){
+    private boolean isValidFilterInput(int choice,String target){
+        Set<String> validProtocols = Set.of("TCP","UDP");
+        Set<String> validStatus = Set.of("SUCCESS","FAILED");
+
         try {
-            int port = Integer.parseInt(target);
-            return port>=0 && port<= 65535;
-        }
-        catch (NumberFormatException e){
+            if (choice == 4) {
+                int port = Integer.parseInt(target);
+                return port >= 0 && port <= 65535;
+            }
+        }catch (NumberFormatException e){
             return false;
         }
+
+        if (choice == 3) return validProtocols.contains(target.toUpperCase());
+        if(choice == 5) return validStatus.contains(target.toUpperCase());
+        if (choice == 1 || choice == 2) return validator.isValid(target);
+
+        return false;
     }
     public void filterResults(List<NetworkLog> lst,LogFilter logFilter, int choice, String target){
         List<NetworkLog> filteredLogLst = logFilter.filter(lst,choice,target);
         System.out.println("\n========== Filter Results ==========");
         System.out.println("\nLogs Found: "+ filteredLogLst.size());
         System.out.println();
+
         for (NetworkLog log : filteredLogLst) {
             LocalDateTime dateTime = log.getTimestamp();
             String sourceIp = log.getSourceIp();
